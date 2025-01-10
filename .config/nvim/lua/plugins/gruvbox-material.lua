5e584ec517cc82f25cc84d311f13c942e4617f11
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-01-10 19:52:51 +0100

    update nvim

--- .config/nvim/lua/plugins/gruvbox-material.lua
+++ .config/nvim/lua/plugins/gruvbox-material.lua
@@ -5,6 +5,7 @@ return {
     config = function()
         vim.g.gruvbox_material_enable_italic = true
         vim.cmd.colorscheme('gruvbox-material')
+        vim.api.nvim_set_hl(0, 'FloatBorder', { link = 'Normal' })
     end
 }
 
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,66 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "bashls", "clangd", "cssls", "ts_ls", "pylsp", "jinja_lsp", "harper_ls" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.bashls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities
+            })
+            lspconfig.cssls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.pylsp.setup({
+                capabilities = capabilities
+            })
+            lspconfig.jinja_lsp.setup({
+                capabilities = capabilities
+            })
+            lspconfig.harper_ls.setup({
+                capabilities = capabilities
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            })
+            require("telescope").load_extension("ui-select")
+        end
+    }
+}
