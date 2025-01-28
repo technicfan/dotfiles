0726f80194a2c6b8c86adbdeb91bc9b2be7e0e3c
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-01-28 10:12:43 +0100

    change swenv keybinding

--- .config/nvim/lua/plugins/swenv.lua
+++ .config/nvim/lua/plugins/swenv.lua
@@ -4,6 +4,6 @@ return {
         require("swenv").setup({
             venvs_path = vim.fn.expand('~/.venv'),
         })
-        vim.keymap.set("n", "<leader>vs", require("swenv.api").pick_venv)
+        vim.keymap.set("n", "<leader>lv", require("swenv.api").pick_venv)
     end
 }
