6a203c8c9d485eacf65b685e7f9cfde7817e24d7
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-02-01 23:03:51 +0100

    add colorizer + tetris

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -35,8 +35,8 @@ return {
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
-                        { name = 'buffer' },
-                    })
+                    { name = 'buffer' },
+                })
             })
         end
     },
