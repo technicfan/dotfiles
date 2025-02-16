6700d0fb7d19faf96eac6286fbcaaebf73a89a70
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-02-16 20:41:32 +0100

    neovide config + better copy/paste

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -11,6 +11,43 @@ vim.cmd("vnoremap > >gv")
 vim.cmd("vnoremap < <gv")
 vim.cmd("vnoremap <Tab> >gv")
 vim.cmd("vnoremap <S-Tab> <gv")
-vim.cmd("vnoremap <C-y> \"+y")
 vim.cmd("tnoremap <Esc> <C-\\><C-n>")
+vim.cmd("vmap <CS-c> \"+y")
+vim.cmd("nmap <CS-v> \"+p")
+vim.cmd("inoremap <CS-v> <C-r>+")
+vim.cmd("cnoremap <CS-v> <C-r>+")
+vim.cmd("inoremap <C-r> <C-v>")
 vim.g.mapleader = " "
+
+-- neovide
+vim.o.guifont = "VictorMono Nerd Font:h11"
+if vim.g.neovide then
+    vim.g.neovide_padding_top = 4
+    vim.g.neovide_padding_bottom = 4
+    vim.g.neovide_padding_right = 4
+    vim.g.neovide_padding_left = 4
+    vim.g.neovide_hide_mouse_when_typing = true
+    vim.g.neovide_cursor_animation_length = 0.05
+    vim.g.neovide_cursor_trail_size = 0.01
+    vim.g.neovide_cursor_animate_command_line = true
+    vim.g.neovide_scroll_animation_length = 0.15
+    vim.g.neovide_floating_blur_amount_x = 0
+    vim.g.neovide_floating_blur_amount_y = 0
+    vim.g.neovide_cursor_unfocused_outline_width = 0.075
+    vim.g.neovide_underline_stroke_scale = 2.5
+end
+
+-- kitty
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+    group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
+    callback = function()
+        io.stdout:write("\x1b]1337;SetUserVar=in_editor=MQo\007")
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+    group = vim.api.nvim_create_augroup("KittyUnsetVarVimLeave", { clear = true }),
+    callback = function()
+        io.stdout:write("\x1b]1337;SetUserVar=in_editor\007")
+    end,
+})
