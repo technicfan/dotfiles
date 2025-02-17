db36aaac11aca86e88ee50c9de4f716575d63c7c
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-02-17 01:40:44 +0100

    fixes

--- .config/nvim/lua/plugins/indent.lua
+++ .config/nvim/lua/plugins/indent.lua
@@ -1,5 +1,23 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {},
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        config = function ()
+            local opts = {}
+            if vim.g.neovide then
+                opts.scope = {
+                    show_start = false,
+                    show_end = false
+                }
+            end
+            require("ibl").setup(opts)
+        end
+    },
+    {
+        "m4xshen/smartcolumn.nvim",
+        opts = {
+            colorcolumn = "128",
+            disabled_filetypes = { "", "help", "text", "markdown", "lazy", "neo-tree", "alpha" }
+        }
+    },
 }
