92cd80c0073749027ae0dc102f3124066b0d0321
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-02-18 21:39:48 +0100

    git stuff (refusing to look at lazygit)

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,58 +1,56 @@
 return {
-    -- {
-    --     "tanvirtin/vgit.nvim",
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim",
-    --         "nvim-tree/nvim-web-devicons"
-    --     },
-    --     event = 'VimEnter',
-    --     config = function()
-    --         local vgit = require("vgit")
-    --         vgit.setup({
-    --             settings = {
-    --                 live_blame = {
-    --                     enabled = false
-    --                 }
-    --             }
-    --         })
-    --
-    --         -- faster live blame
-    --         vim.o.updatetime = 300
-    --
-    --         vim.keymap.set("n", "<leader>gp", vgit.project_diff_preview, {})
-    --         vim.keymap.set("n", "<leader>gl", vgit.project_logs_preview, {})
-    --         vim.keymap.set("n", "<leader>gd", vgit.buffer_hunk_preview, {})
-    --         vim.keymap.set("n", "<leader>hr", vgit.buffer_hunk_reset, {})
-    --         vim.keymap.set("n", "<leader>gh", vgit.buffer_history_preview, {})
-    --         vim.keymap.set("n", "<leader>ll", vgit.toggle_live_blame, {})
-    --         vim.keymap.set("n", "<leader>lp", vgit.buffer_blame_preview, {})
-    --     end
-    -- }
     {
-        "chrisgrieser/nvim-tinygit",
-        tag = "v0.9",
-        dependencies = "stevearc/dressing.nvim",
-        config = function ()
-            require("tinygit").setup({
-                stage = {
-                    telescopeOpts = {
-                        layout_strategy = "horizontal",
-                        layout_config = {
-                            horizontal = {
-                                preview_width = 0.65,
-                                height = { 0.7, min = 20 },
-                            },
-                        },
-                        winhighlight = "FloatBorder:Normal,NormalFloat:Normal"
+        "tanvirtin/vgit.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "lewis6991/gitsigns.nvim"
+        },
+        event = 'VimEnter',
+        config = function()
+            local vgit = require("vgit")
+            vgit.setup({
+                settings = {
+                    live_blame = {
+                        enabled = false
                     },
-                },
-                commit = {
-                    preview = true
+                    live_gutter = {
+                        enabled = false
+                    }
                 }
             })
-            vim.keymap.set("n", "<leader>ga", function() require("tinygit").interactiveStaging() end, { desc = "git add" })
-            vim.keymap.set("n", "<leader>gc", function() require("tinygit").smartCommit() end, { desc = "git commit" })
-            vim.keymap.set("n", "<leader>gp", function() require("tinygit").push() end, { desc = "git push" })
+
+            vim.keymap.set("n", "<leader>ga", vgit.project_diff_preview, {})
+            vim.keymap.set("n", "<leader>gc", vgit.project_commit_preview, {})
+            vim.keymap.set("n", "<leader>gl", vgit.project_logs_preview, {})
+            vim.keymap.set("n", "<leader>hp", vgit.buffer_hunk_preview, {})
+            vim.keymap.set("n", "<leader>gh", vgit.buffer_history_preview, {})
+            vim.keymap.set("n", "<leader>lp", vgit.buffer_blame_preview, {})
         end
     },
+    {
+        "chrisgrieser/nvim-tinygit",
+        dependencies = "stevearc/dressing.nvim",
+        config = function ()
+            require("tinygit").setup()
+            vim.keymap.set("n", "<leader>gp", require("tinygit").push)
+        end
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function ()
+            require("gitsigns").setup()
+            vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>ll", "<cmd>Gitsigns toggle_current_line_blame<CR>", {})
+            vim.keymap.set("n", "<leader>gs", function ()
+                local status = vim.b.gitsigns_status_dict
+                if status and not (status.added == 0 and status.changed == 0 and status.removed == 0) then
+                    require("gitsigns").stage_buffer()
+                else
+                    require("gitsigns").reset_buffer_index()
+                end
+            end)
+        end
+    }
 }
