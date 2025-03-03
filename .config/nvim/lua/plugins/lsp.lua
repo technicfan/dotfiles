696f0226f28b186386a3e05a6677c54b8d2dab51
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-03-03 15:54:05 +0100

    c for lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -51,8 +51,8 @@ return {
             })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, {})
+            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
         end
     }
