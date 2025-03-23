10dd14e9107fc1b2ba19d09d60fee0cdb807c9b4
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-03-23 22:39:36 +0100

    change

--- .config/nvim/lua/plugins/pairs.lua
+++ .config/nvim/lua/plugins/pairs.lua
@@ -16,7 +16,7 @@ return {
         config = function ()
             require("nvim-ts-autotag").setup({
                 opts = {
-                    enable_close = false,
+                    enable_close = true,
                     enable_rename = true,
                     enable_close_on_slash = true
                 }
